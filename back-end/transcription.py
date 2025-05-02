88bd2d1aa0d316768048c98f24862fe03411e802
Baastheglass <lightningblazer123@gmail.com>
2025-05-03 00:12:39 +0500

    pushing logs

--- back-end/transcription.py
+++ back-end/transcription.py
@@ -203,14 +203,15 @@ class OpenAITranscriber:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
         if(data['type'] == "session.created"):
-            event = {
-                "type": "session.update",
-                "session": {
-                            "instructions": "Your job is to transcribe what I say and return a transcription mirroring exactly what I said. Do not answer the question. Do not add any extra information. Just transcribe what the audio is narrating."
-                }
-            }
-            if(self.websocket_working("openai")):
-                self.openai_ws.send(json.dumps(event))
+            pass
+            # event = {
+            #     "type": "session.update",
+            #     "session": {
+            #                 "instructions": "Your job is to transcribe what I say and return a transcription mirroring exactly what I said. Do not answer the question. Do not add any extra information. Just transcribe what the audio is narrating."
+            #     }
+            # }
+            # if(self.websocket_working("openai")):
+            #     self.openai_ws.send(json.dumps(event))
             
         elif(data['type'] == "session.updated" and self.sent_audio == False):    
             pass
